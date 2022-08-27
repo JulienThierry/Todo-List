2c95bc9ad214180bc446ef0d9d49f28cfcdd3e58
Julien Thierry <julienthierrypro@gmail.com>
2022-08-27 12:42:34 +0200

    Local storage + css

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const form = document.querySelector("form");
 
+const storeList = () => {
+  window.localStorage.todoStorage = todoList.innerHTML;
+};
+
+const getList = () => {
+  todoList.innerHTML = window.localStorage.todoStorage;
+};
+
+getList();
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (todoInput.value) {
     todoList.innerHTML += `<li>${todoInput.value}</li>`;
   }
   todoInput.value = null;
+  storeList();
 });
 
 todoList.addEventListener("click", (e) => {
-  e.target.remove();
+  if (e.target.classList != "") {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+  }
+  storeList();
 });
